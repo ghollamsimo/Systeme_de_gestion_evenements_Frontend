4ff23a71299b84d3680ca1bf5f6876c64c8aa5f1
ghollamsimo <ghollamsimo1@gmail.com>
2024-11-26 14:03:46 +0100

    Feat --> Adding LoginAuth.test in folder src/__test__

---
+++ src/__test__/LoginAuth.test.ts
@@ -0,0 +1,10 @@
+import React from "react";
+import { render, fireEvent } from "@testing-library/react";
+
+import Login from "../auth/LoginAuth.tsx";
+
+describe("Test Authentication", () => {
+    test("should display a blank login form", async () => {
+
+    });
+})
