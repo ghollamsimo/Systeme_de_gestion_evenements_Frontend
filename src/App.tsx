0a0e2146d28a1e8c43fcde8aeb522971071e3201
ghollamsimo <ghollamsimo1@gmail.com>
2024-11-27 22:08:05 +0100

    Feat --> Adding route of home page

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import './App.css'
 import {Route, Routes} from "react-router-dom";
 import Login from "./auth/LoginAuth.tsx";
+import {HomePage} from "./pages/HomePage.tsx";
 
 function App() {
 
@@ -8,6 +9,7 @@ function App() {
     <>
         <Routes>
             <Route path="/" element={<Login/>}/>
+            <Route path='/home' element={<HomePage/>}/>
         </Routes>
     </>
   )
