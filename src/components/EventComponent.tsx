0bfb196ae0d9d10e9ad12c9328886d0227919bd8
ghollamsimo <ghollamsimo1@gmail.com>
2024-11-27 22:06:07 +0100

    Feat --> Adding EventComponent with cards

--- src/components/EventComponent.tsx
+++ src/components/EventComponent.tsx
@@ -1,7 +1,18 @@
 import React from "react";
+import Img from "../assets/poster.jpg";
 
-const Event: React.FC = () => {
-  return(
-      <></>
-  )
-}
+
+export const Event: React.FC = () => {
+    return (
+        <div className="max-w-sm rounded overflow-hidden  bg-white">
+            <img
+                className="w-full h-48 object-cover"
+                src={Img}
+            />
+            <div className="px-6 py-4">
+                <h2 className="font-bold text-xl mb-2">Exciting Event</h2>
+                <p className="text-gray-700 text-base">Join us for an amazing event filled with fun and excitement!</p>
+            </div>
+        </div>
+    );
+};
