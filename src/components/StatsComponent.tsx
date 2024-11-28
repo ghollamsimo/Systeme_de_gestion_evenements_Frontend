32a15ea8455bd520f0e0c5b45efe3d40accd6a51
ghollamsimo <ghollamsimo1@gmail.com>
2024-11-28 16:05:07 +0100

    Feat --> Adding stats props in StatsComponent

--- src/components/StatsComponent.tsx
+++ src/components/StatsComponent.tsx
@@ -1,22 +1,16 @@
 import React from "react";
 
-export const Stats: React.FC = () => {
-    const statistics = [
-        { title: "Total Events", count: 245 },
-        { title: "Total Users", count: 1287 },
-    ];
+export const Stats: React.FC = ({stats}) => {
   return(
       <>
           <div className="grid grid-cols-1 sm:grid-cols-2  gap-4 mb-6">
-              {statistics.map((stat, index) => (
+
                   <div
-                      key={index}
                       className="bg-white w-full shadow-md rounded-lg p-4 flex items-center justify-between"
                   >
-                      <h2 className="text-gray-600 font-medium">{stat.title}</h2>
-                      <span className="text-2xl font-bold text-purple-600">{stat.count}</span>
+                      <h2 className="text-gray-600 font-medium">Total Events</h2>
+                      <span className="text-2xl font-bold text-purple-600">{stats}</span>
                   </div>
-              ))}
           </div>
       </>
   )
