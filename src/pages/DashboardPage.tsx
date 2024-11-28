e37c16e1f4f4dce6ccbff3d28c10227041ca2816
ghollamsimo <ghollamsimo1@gmail.com>
2024-11-28 16:04:34 +0100

    Feat --> Adding dispatch of stats method

--- src/pages/DashboardPage.tsx
+++ src/pages/DashboardPage.tsx
@@ -1,17 +1,26 @@
-import React from "react";
+import React, {useEffect} from "react";
 import {EventTable} from "../tables/EventTable.tsx";
 import {NavbarDashboard} from "../components/NavbarDashboardComponent.tsx";
 import {Stats} from "../components/StatsComponent.tsx";
 import {UsersTable} from "../tables/UsersTabel.tsx";
+import {useNavigate} from "react-router-dom";
+import {useDispatch, useSelector} from "react-redux";
+import {RootState} from "../redux/Store.ts";
+import {stats} from "../redux/slices/EventSlice.ts";
 
 export const DashboardPage: React.FC = () => {
+    const dispatch = useDispatch();
+    const stat = useSelector((state: RootState) => state.event.dataObj);
 
+    useEffect(() =>{
+        dispatch(stats())
+    }, [dispatch])
     return (
         <div className="min-h-screen">
             <main className="flex-1">
              <NavbarDashboard/>
                 <div className="p-6">
-                    <Stats/>
+                    <Stats stats={stat}/>
                     <EventTable/>
                     <UsersTable/>
                 </div>
